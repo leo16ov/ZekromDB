532f6d7458653aebad376d70f5b092a7faa05bff
leo16ov <113075189+leo16ov@users.noreply.github.com>
2025-10-18 18:22:57 -0300

    Add Main class to start the Servidor

---
+++ src/servidor/Main.java
@@ -0,0 +1,11 @@
+
+package servidor;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Servidor servidor = new Servidor();
+        servidor.iniciar();
+    }
+    
+}
